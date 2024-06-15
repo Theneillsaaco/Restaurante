7ec9076c079e0251eee5bf7a032c93e4223a0d75
Theneillsaaco <isaacdepena18@gmail.com>
2024-06-14 20:19:43 -0400

    Add IOC proyect

---
+++ Restaurante.IOC/Dependencies/RepositoriesDependency.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Restaurante.Domain.Interfaces;
+using Restaurante.Infrastructure.Repository;
+
+namespace Restaurante.IOC.Dependencies;
+
+public static class RepositoriesDependency
+{
+    public static void AddRepositoriesDependency(this IServiceCollection service)
+    {
+        // Repository services
+        
+        service.AddScoped<IClienteRepository, ClienteRepository>();
+        service.AddScoped<IDetallePedidoRepository, DetallePedidoRepository>();
+        service.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
+        service.AddScoped<IFacturaRepository, FacturaRepository>();
+        service.AddScoped<IMenuRepository, MenuRepository>();
+        service.AddScoped<IMesaRepository, MesaRepository>();
+        service.AddScoped<IPedidoRepository, PedidoRepository>();
+        
+        // Services...
+    }
+}
