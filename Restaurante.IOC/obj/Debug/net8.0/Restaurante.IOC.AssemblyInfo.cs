da6460f39a8454ae1d8688490b9562094db2a647
Theneillsaaco <isaacdepena18@gmail.com>
2024-06-17 20:59:57 -0400

    a

--- Restaurante.IOC/obj/Debug/net8.0/Restaurante.IOC.AssemblyInfo.cs
+++ Restaurante.IOC/obj/Debug/net8.0/Restaurante.IOC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante.IOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc0caa3350472091e25b185b73ae0cfa94f05b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a91ce9bf7bd06ab02e7ba29017c2acf87fa3ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante.IOC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurante.IOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
